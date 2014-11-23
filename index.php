74133e57e19e35b1f79cf0102c561a4f5d5c8a6a
Ian Carroll <him@ian.sh>
2014-11-22 20:08:20 -0500

    include()

--- index.php
+++ index.php
@@ -6,11 +6,12 @@
   <meta name="description" content="Welcome to Hackathon Hackers. Learn about hackathons here!"> <!-- yeah, very creative I know -->
   <meta name="keywords" content="facebook,hh,hackathon,hackathons,fb hh,hackathon hackers,hackathon hacker,hackathon fb,fb hackathons,learn hackathons,what is a hackathon,about hackathons"
   <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.0/cosmo/bootstrap.min.css" rel="stylesheet">
   <link href="/assets/css/primary.css" rel="stylesheet"> <!-- CDN this in production -->
 </head>
 <body>
   <div class="container">
-    
+    <?php $page = "home"; include("templates/nav.php"); ?>
   </div>
 <!-- JS -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
